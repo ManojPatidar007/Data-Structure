bd5044eaba0afdd029681612b7cb96e9f6711b43
Manoj Patidar <41301259+ManojPatidar007@users.noreply.github.com>
2019-03-03 19:57:51 +0530

    Update EndsWith.java

--- EndsWith.java
+++ EndsWith.java
@@ -1,7 +1,9 @@
 public class EndsWith{  
 public static void main(String args[]){  
 String s1="java by javatpoint";  
-System.out.println(s1.endsWith("t"));  
-System.out.println(s1.endsWith("point"));
-System.out.println(s1.endsWith("java"));   
-}}  
+System.out.println(s1.endsWith("t")); //try to write comment as output 
+System.out.println(s1.endsWith("point"));//means what this line will print
+System.out.println(s1.endsWith("java"));//or if try to write doun comment at the bottem so that whin you read it will help you
+  //to understend what this methow will do.
+}
+}  
